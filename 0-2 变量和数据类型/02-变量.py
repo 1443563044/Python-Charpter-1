6dcf6b5453313f60344af577f9ad7236dea86855
1443563044 <65066028+1443563044@users.noreply.github.com>
2023-06-22 03:01:37 +0800

    Create 02-变量.py

---
+++ 0-2 变量和数据类型/02-变量.py
@@ -0,0 +1,16 @@
+""""
+1.定义变量方法
+    变量名字 = 值
+"""
+
+my_name = 'TOM'
+print('我的名字是' , my_name)
+
+schoolName = '我是隔壁老王'
+print(schoolName)
+
+schoolName = '我在哪我是谁'
+print(schoolName)
+
+greeting = '你好'
+print(greeting)
